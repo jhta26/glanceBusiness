75eb8537a212c8c301c78f3e219849a8359eccd2
jhta26 <hsujasonf@gmail.com>
2018-04-19 00:44:50 -0700

    first commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function HeaderComponent() {
+  return (
+   <nav>
+    <div class="nav-wrapper header #263238 red accent-3">
+      <a href="#" class="brand-logo center">Glance Business</a>
+    </div>
+  </nav>
+  );
+}
